3a6c03ae458aab8f724c7e56dbc877f8df7b2084
Keegan H <keeganslane@gmail.com>
2019-02-16 18:34:11 -0800

    removed build script written in bash

--- build.sh
+++
@@ -1,7 +0,0 @@
-
-cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-cat templates/top.html content/about.html templates/bottom.html > docs/about.html
-cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
-cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
-cat templates/top.html content/music.html templates/bottom.html > docs/music.html
-cat templates/top.html content/photography.html templates/bottom.html > docs/photography.html
