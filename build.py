da7b1bce0df059f4953a166ff562a0734afa8aa1
Keegan H <keeganslane@gmail.com>
2019-02-17 18:10:30 -0800

    updating

--- build.py
+++ build.py
@@ -31,10 +31,6 @@ def main():
             "title": "Projects",
         },
     ]
-<<<<<<< HEAD
-
-=======
->>>>>>> c611972dd69d368b9236d4642cfe4ebaafa3e0a0
     for page in pages:
         page_title = page["title"]
         openfile = page["filename"]
@@ -44,18 +40,8 @@ def main():
         finished_content_pages = template.replace("{{content}}", page_content)
         open(output_file, "w+").write(finished_content_pages)
 
-<<<<<<< HEAD
 
 print("I ran")
 
-=======
-# test script ran
-print("I ran")
-
-# Create new function to add page to dict.
-# def add_page():
-
-
->>>>>>> c611972dd69d368b9236d4642cfe4ebaafa3e0a0
 if __name__ == "__main__":
     main()
