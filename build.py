afb8f3f53db8a004ae75b3e891d1d8c6d5db6858
keegan-h <keeganslane@gmail.com>
2019-02-18 18:05:25 -0800

    updated formatting of build.py and put main() at bottom of doc

--- build.py
+++ build.py
@@ -1,62 +1,71 @@
-#def main():
+# def main():
+pages = [
+    {
+        "filename": "content/index.html",
+        "output": "docs/index.html",
+        "title": "Home",
+    },
+    {
+        "filename": "content/about.html",
+        "output": "docs/about.html",
+        "title": "About Me",
+    },
+    {
+        "filename": "content/contact.html",
+        "output": "docs/contact.html",
+        "title": "Contact",
+    },
+    {
+        "filename": "content/music.html",
+        "output": "docs/music.html",
+        "title": "Music",
+    },
+    {
+        "filename": "content/photography.html",
+        "output": "docs/photography.html",
+        "title": "Photography",
+    },
+    {
+        "filename": "content/projects.html",
+        "output": "docs/projects.html",
+        "title": "Projects",
+    },
+]
+
+
 def read_pages():
-    """ docstring for function"""
-    pages = [
-            {
-                "filename": "content/index.html",
-                "output": "docs/index.html",
-                "title": "Home",
-            },
-            {
-                "filename": "content/about.html",
-                "output": "docs/about.html",
-                "title": "About Me",
-            },
-            {
-                "filename": "content/contact.html",
-                "output": "docs/contact.html",
-                "title": "Contact",
-            },
-            {
-                "filename": "content/music.html",
-                "output": "docs/music.html",
-                "title": "Music",
-            },
-            {
-                "filename": "content/photography.html",
-                "output": "docs/photography.html",
-                "title": "Photography",
-            },
-            {
-                "filename": "content/projects.html",
-                "output": "docs/projects.html",
-                "title": "Projects",
-            },
-        ]
+    """read pages """
     for page in pages:
-        page_title = page["title"]
-        openfile = page["filename"]
-        output_file = page["output"]
-        #template = open("templates/base.html").read()
-        page_content = open(openfile).read()
-        #finished_content_pages = template.replace("{{content}}", page_content)
-        #open(output_file, "w+").write(finished_content_pages)
-        return
-read_pages()
+    page_title = page["title"]
+    openfile = page["filename"]
+    output_file = page["output"]
+    #template = open("templates/base.html").read()
+    page_content = open(openfile).read()
+    #finished_content_pages = template.replace("{{content}}", page_content)
+    #open(output_file, "w+").write(finished_content_pages)
+
 
 def apply_template(content):
-    template = open("templates/base.html").read() # Read in template
-    finished_content_pages = template.replace("{{content}}", page_content) #string replacing
+    template = open("templates/base.html").read()  # Read in template
+    finished_content_pages = template.replace(
+        "{{content}}", page_content)  # string replacing
     open(output_file, "w+").write(finished_content_pages)
     return results
+
 
 def main():
     content = open("docs/index.html")
     resulting_html_for_index = apply_template(content)
-# test script ran
-print("I ran")
-# Create new function to add page to dict.
-# def add_page():
+    # test script ran
+    print("I ran")
+    # Create new function to add page to dict.
+    # def add_page():
+
+
+def main():
+
+
+read_pages()
 
 if __name__ == "__main__":
     main()
